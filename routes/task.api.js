be7042032123df4d2cc6e7660d4fba7e334af303
Nguyen Doan <nguyendoan3456@gmail.com>
2024-02-21 09:32:01 -0500

    add all the route for task

--- routes/task.api.js
+++ routes/task.api.js
@@ -1,18 +1,58 @@
 const express = require("express");
-const {
-  getAllTasks,
-  createTask,
-  updateTaskById,
-  deleteTaskById,
-} = require("../controllers/task.controller");
 const router = express.Router();
+const {
+  createTask,
+  getAllTasks,
+  addAssignee,
+  getTaskById,
+  updateTaskStatusById,
+  deleteTaskById,
+} = require("../controllers/task.controllers.js");
 
+/**
+ * @route GET api/tasks
+ * @description get list of task
+ * @access public
+ */
 router.get("/", getAllTasks);
 
+/**
+ * @route GET api/tasks
+ * @description get list of task by id
+ * @access public
+ */
+router.get("/:taskId", getTaskById);
+
+//Create
+/**
+ * @route POST api/tasks
+ * @description create a task
+ * @access public
+ */
 router.post("/", createTask);
 
-router.put("/:id", updateTaskById);
+//Update
+/**
+ * @route PUT api/tasks
+ * @description add people into a task
+ * @access public
+ */
+router.put("/assign/:taskId", addAssignee);
 
-router.delete("/:id", deleteTaskById);
+//Update
+/**
+ * @route PUT api/tasks
+ * @description update reference to a task
+ * @access public
+ */
+router.put("/:taskId", updateTaskById);
+
+//delete
+/**
+ * @route PUT api/tasks
+ * @description delete  a task
+ * @access public
+ */
+router.delete("/:taskId", deleteTaskById);
 
 module.exports = router;
